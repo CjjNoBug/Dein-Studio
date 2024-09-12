1637b7afeb413afef15b42f22e92558a53b53e3d
Caojj <15760348823@163.com>
2024-09-12 13:11:56 +0800

    project structure

---
+++ src/main/java/com/deinstudio/deinstudio/DeinStudioApplication.java
@@ -0,0 +1,13 @@
+package com.deinstudio.deinstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeinStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeinStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deinstudio/deinstudio/DeinStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deinstudio.deinstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeinStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
